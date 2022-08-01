22bffad71aa687323c907f0058eaa9cad1ea2518
Solomonaz <solomonaz2022@gmail.com>
2022-08-01 17:18:07 +0300

    Variation model added

--- carts/views.py
+++ carts/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from  .models import Cart, CartItem
-from store.models import Product
+from store.models import Product, Variation
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 
 def _cart_id(request):
-    cart     =request.session.session_key
+    cart = request.session.session_key
     if not cart:
         cart    =request.session.create()
     return cart
 
 def add_cart(request, product_id):
-    product     =Product.objects.get(id=product_id) #get the product
+    product = Product.objects.get(id=product_id) #get the product
+    product_variation = []
+    if request.method =='POST':
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+
+            try:
+                variation = Variation.objects.get(product = product, variation_category__iexact = key, variation_value__iexact = value)
+                product_variation.append(variation)
+            except:
+                pass
+
     try:
         cart = Cart.objects.get(cart_id = _cart_id(request)) #get the cart using the cart_id present in the session
     except Cart.DoesNotExist:
@@ -19,18 +32,44 @@ def add_cart(request, product_id):
         )
     cart.save()
 
-    try:
-        cart_item = CartItem.objects.get(product=product,cart=cart)
-        cart_item.quantity+=1
-        cart_item.save()
-    except CartItem.DoesNotExist:
+    is_cart_item_exists = CartItem.objects.filter(product=product, cart=cart).exists()
+    if is_cart_item_exists:
+        cart_item = CartItem.objects.filter(product=product, cart=cart)
+        # existing_variation -> database
+        # current_variation -> product_variation
+        # item_id -> database
+        ex_var_list = []
+        id = []
+        for item in cart_item:
+            existing_variation = item.variations.all()
+            ex_var_list.append(list(existing_variation))
+            id.append(item.id)
+
+        print(ex_var_list)
+
+        if product_variation in ex_var_list:
+            # increase the cart item quantitu
+            index = ex_var_list.index(product_variation)
+            item_id = id[index]
+            item = CartItem.objects.get(product=product, id=item_id)
+            item.quantity +=1
+            item.save()
+        else:
+            # create new cart item
+            item = CartItem.objects.create(product=product,quantity=1,cart=cart)
+            if len(product_variation) > 0:
+                item.variations.clear()
+                item.variations.add(*product_variation)
+            cart_item.save()
+    else:
         cart_item = CartItem.objects.create(
             product = product,
             quantity = 1,
-            cart = cart,
-            
+            cart = cart, 
         )
-          
+        if len(product_variation) > 0:
+            cart_item.variations.clear()
+            cart_item.variations.add(*product_variation)
         cart_item.save()
     return redirect('cart')
 
@@ -54,6 +93,8 @@ def remove_cart_item(request, product_id):
 
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
+        tax =0
+        grand_total = 0
         cart = Cart.objects.get(cart_id = _cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
@@ -61,7 +102,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
             quantity += cart_item.quantity
         tax = (2*total)/100
         grand_total = total + tax
-    except CartItem.DoesNotExist:
+    except ObjectDoesNotExist:
         pass
 
     context = {
